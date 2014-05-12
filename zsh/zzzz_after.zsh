0a19de4de4d943a6891ab62ee4619f48613915f8
David Pelaez <david@vlipco.co>
2014-05-12 11:58:20 -0500

    imrpveoment in prompt for better integration with tmux. Adds tmux config too

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,3 +1,8 @@
+# In normal mode, allow escape to switch to insert
+# using `a` (adding next to cursor)
+bindkey -M vicmd  '\e' vi-add-next
+#DISABLE_AUTO_TITLE=true
+
 # Load any custom after code
 if [ -d $HOME/.zsh.after/ ]; then
   if [ "$(ls -A $HOME/.zsh.after/)" ]; then
